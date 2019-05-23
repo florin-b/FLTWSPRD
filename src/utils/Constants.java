cfc88dde2adad586f875c5f7ce4b671d634acd1c
florin-b <gflorinb@yahoo.com>
2019-05-23 10:06:57 +0300

    Update Constants.java

--- src/utils/Constants.java
+++ src/utils/Constants.java
@@ -8,37 +8,7 @@ public class Constants {
 	public static double VITEZA_MINIMA_PLECARE_CLIENT = 10;
 	public static double VITEZA_MINIMA_PLECARE_FILIALA = 20;
 	public static String BEANS_XML = "mybeans.xml";
-	public static String GOOGLE_MAPS_API_KEY = "AIzaSyAmslp9dA-gCEl18PYfxkvZ-BqUFp4Pi3o";
 
-	public static String GOOGLE_MAPS_API_KEY_DIST = "AIzaSyAs60CiMm4MiFySEFpVBhE67NhtCrr93LM";
-
-	// public static String GOOGLE_MAPS_API_KEY_DIST =
-	// "AIzaSyBDO8739CUoZY7ZJ-IVL-PvDXZwKHqpb_Q";
-
-	public static String GOOGLE_MAPS_API_KEY_REV_GEO = "AIzaSyDuZrxbUtw7A0w15dqinnxOUcmfieGe-Kk";
-
-	// developer6.arabesque@gmail.com/1DeveLo@er2
-	public static String GOOGLE_MAPS_API_KEY_01 = "AIzaSyCeqgAiv72BnAWxR8HyKk_pU8ZSax1rZIU";
-	public static String GOOGLE_MAPS_API_KEY_02 = "AIzaSyAddwZS16diBFO3KdaUWOQ6QoiLwXasGdc";
-	public static String GOOGLE_MAPS_API_KEY_03 = "AIzaSyCtChm0ZpaC9yaFxGyeHUoscszflVy4dJM";
-	public static String GOOGLE_MAPS_API_KEY_04 = "AIzaSyDpCUh3QS1y5R8wfdL9N_Tm9_nLDEmMtuo";
-	public static String GOOGLE_MAPS_API_KEY_05 = "AIzaSyCHGgWJLtudUe2h-efqdlBOz5gt2so8V1Q";
-	public static String GOOGLE_MAPS_API_KEY_06 = "AIzaSyBOCWWYrepaIryyZcvVMWdgAAIzmqN7Sms";
-	public static String GOOGLE_MAPS_API_KEY_07 = "AIzaSyDuNvs4lfKn1xQrPLX-wPg_KA5b31e78a0";
-	
-	
-	// developer7.arabesque@gmail.com/1DeveLo@er2
-	public static String GOOGLE_MAPS_API_KEY_DIST_01 = "AIzaSyCkdsKPPfZlzs9prBhT28S9hSWHRkrNsxY";
-	public static String GOOGLE_MAPS_API_KEY_DIST_02 = "AIzaSyAEyXJnbXNlmvXaSif3yrKS4BoA0c73b4o";
-	public static String GOOGLE_MAPS_API_KEY_DIST_03 = "AIzaSyDPt3Z9OLWtpNqRaw6LomR_nplxSjFunYY";
-	public static String GOOGLE_MAPS_API_KEY_DIST_04 = "AIzaSyA-cHTlLK_e8ekBZGxA-qk9tcIBaqs6ngM";
-	public static String GOOGLE_MAPS_API_KEY_DIST_05 = "AIzaSyAnSogkPYvLWBP_5Umh6GnEuM458aQY2TE";
-	public static String GOOGLE_MAPS_API_KEY_DIST_06 = "AIzaSyD5GhVs4E5uRl2b9cqSU-yi8xPgu63Ms-8";	
-	public static String GOOGLE_MAPS_API_KEY_DIST_07 = "AIzaSyBi50cw20JpOQtm7y3MYeIS8iCV76u4AQY";
-	
-	public static String GOOGLE_MAPS_API_KEY_DIST_LOC_01 = "AIzaSyAt00YhmNdwGqpB-maQZ7ezUstaZEvsOF0";
-	public static String GOOGLE_MAPS_API_KEY_DIST_LOC_02 = "AIzaSyCYqYaJPfI5F85KeSYizOhY93W9bUgRANw";
-	public static String GOOGLE_MAPS_API_KEY_DIST_LOC_03 = "AIzaSyAs9oSS4YySKlq71-LzPgL2gAx_jPRkui8";
 	
 	
 
